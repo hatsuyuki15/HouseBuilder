25ea6d0f6dd4b227193e4ca923b50dfb316eb4be
hatsuyuki15 <hatsuyuki15@gmail.com>
2015-05-20 11:22:12 +0700

    + add obj.cpp

---
+++ House Builder/Object.cpp
@@ -0,0 +1,35 @@
+#include "Common.h"
+#include <algorithm>
+
+using namespace std;
+
+Object::Object() {
+	width = height = -1;
+}
+
+GLfloat Object::getWidth() {
+	if (width == -1)
+		calculateSize();
+	return width;
+}
+
+GLfloat Object::getHeight() {
+	if (height == -1)
+		calculateSize();
+	return height;
+}
+
+void Object::calculateSize() {
+	GLfloat minX , minZ, maxX, maxZ;
+	minX = maxX = vertexs[0].x;
+	minZ = maxZ = vertexs[0].z;
+	for (int i = 0; i < vertexs.size(); i++) {
+		vertex& v = vertexs[i];
+		minX = min(minX, v.x);
+		minZ = min(minZ, v.z);
+		maxX = max(maxX, v.x);
+		maxZ = max(maxZ, v.z);
+	}	
+	width = maxX - minX;
+	height = maxZ - minZ;
+}
